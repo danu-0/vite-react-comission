9f8d8dec1a30286c8b96a6ae1b2dfffde75624fb
danu-0 <danuhaeridaputra07@gmail.com>
2024-07-11 22:05:23 +0800

    add:initial file

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import Nav from './Nav'
 import Hero from './Hero'
 import './style/index.css'
 import Content from './Content'
 import Footer from './Footer'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.render(
   <React.StrictMode>
     <Nav />
     <Hero />
     <Content />
     <Footer />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
